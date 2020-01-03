cb00e1b0f6d25a729887a7c96763802ae7b6d3fb
Chuck Pilon <chuck.pilon@gmail.com>
2020-01-03 13:35:53 -0600

    Added defaults for optionals. Removed rds_ prefix from the RDS variables.

--- variables.tf
+++ variables.tf
@@ -38,136 +38,139 @@ variable "availability_zones" {
   description = "Availability zones"
 }
 
-variable "rds_instance_name" {
+variable "instance_name" {
   type        = string
   description = "Name of the RDS instance"
 }
 
-variable "rds_database_name" {
+variable "database_name" {
   type        = string
   description = "(Optional) The name of the database to create when the DB instance is created. If this parameter is not specified, no database is created in the DB instance."
   default     = null
 }
 
-variable "rds_storage_type" {
+variable "storage_type" {
   type        = string
-  description = "(Optional) One of \"standard\" (magnetic), \"gp2\" (general purpose SSD), or \"io1\" (provisioned IOPS SSD). The default is \"io1\" if rds_iops is specified, \"gp2\" if not."
+  description = "(Optional) One of \"standard\" (magnetic), \"gp2\" (general purpose SSD), or \"io1\" (provisioned IOPS SSD). The default is \"io1\" if iops is specified, \"gp2\" if not."
   default     = null
 }
 
-variable "rds_iops" {
+variable "iops" {
   type        = string
   description = "(Optional) The amount of provisioned IOPS. Setting this implies a storage_type of \"io1\"."
   default     = null
 }
 
-variable "rds_instance_class" {
+variable "instance_class" {
   type        = string
   description = "RDS instance class (e.g. db.t2.micro)"
 }
 
-variable "rds_initial_allocated_storage" {
+variable "initial_allocated_storage" {
   type        = number
   description = "The initial storage allocation in gibibytes"
 }
 
-variable "rds_max_allocated_storage" {
+variable "max_allocated_storage" {
   type        = number
   description = "The upper limit to which Amazon RDS can automatically scale the storage of the DB instance. Must be greater than or equal to rds_initial_allocated_storage or 0 to disable Storage Autoscaling."
 }
 
-variable "rds_master_username" {
+variable "master_username" {
   type        = string
   description = "RDS master user name"
 }
 
-variable "rds_master_password" {
+variable "master_password" {
   type        = string
   description = "RDS master user password"
 }
 
-variable "rds_ingress_cidr_blocks" {
+variable "ingress_cidr_blocks" {
   type        = list(string)
   description = "RDS security group ingress CIDR block"
 }
 
-variable "rds_backup_window" {
+variable "backup_window" {
   type        = string
-  description = "The daily time range (in UTC) during which automated backups are created if they are enabled."
+  description = "(Optional) The daily time range (in UTC) during which automated backups are created if they are enabled. Example: \"09:46-10:16\". Must not overlap with maintenance_window."
+  default     = null
 }
 
-variable "rds_maintenance_window" {
+variable "maintenance_window" {
   type        = string
-  description = "The window to perform maintenance in. Syntax: \"ddd:hh24:mi-ddd:hh24:mi\"."
+  description = "(Optional) The window to perform maintenance in. Syntax: \"ddd:hh24:mi-ddd:hh24:mi\". Eg: \"Mon:00:00-Mon:03:00\"."
+  default     = null
 }
 
-variable "rds_cloudwatch_logs_exports" {
+variable "cloudwatch_logs_exports" {
   type        = list(string)
-  description = "List of log types to enable for exporting to CloudWatch logs. Valid values: audit, error, general, slowquery, trace"
+  description = "(Optional) List of log types to enable for exporting to CloudWatch logs. Valid values: audit, error, general, slowquery, trace"
+  default     = null
 }
 
-variable "rds_engine_version" {
+variable "engine_version" {
   type        = string
   description = "The MySQL engine version to use."
 }
 
-variable "rds_option_group_name" {
+variable "option_group_name" {
   type        = string
   description = "Name of the DB option group to associate."
 }
 
-variable "rds_parameter_group_name" {
+variable "parameter_group_name" {
   type        = string
   description = "Name of the DB parameter group to associate."
 }
 
-variable "rds_backup_retention_period" {
+variable "backup_retention_period" {
   type        = number
   description = "The days to retain backups for. Must be between 0 and 35. Must be greater than 0 if the database is used as a source for a Read Replica."
 }
 
-variable "rds_monitoring_interval" {
+variable "monitoring_interval" {
   type        = number
   description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance. To disable collecting Enhanced Monitoring metrics, specify 0. Valid Values: 0, 1, 5, 10, 15, 30, 60."
   default     = 0
 }
 
-variable "rds_availability_zone" {
+variable "availability_zone" {
   type        = string
   description = "(Optional) The AZ for the RDS instance. Leave unset if rds_multi_az is true."
   default     = null
 }
 
-variable "rds_multi_az" {
+variable "multi_az" {
   type        = bool
   description = "Specifies if the RDS instance is multi-AZ"
 }
 
-variable "rds_publicly_accessible" {
+variable "publicly_accessible" {
   type        = bool
   description = "(Optional) Bool to control if instance is publicly accessible. Default is false"
   default     = false
 }
 
-variable "rds_iam_auth" {
+variable "iam_database_authentication_enabled" {
   type        = bool
   description = "(Optional) Specifies whether or mappings of AWS Identity and Access Management (IAM) accounts to database accounts is enabled."
   default     = false
 }
 
-variable "rds_copy_tags_to_snapshot" {
+variable "copy_tags_to_snapshot" {
   type = bool
   description = "(Optional) Copy all Instance tags to snapshots. Default is false."
   default = false
 }
 
-variable "rds_skip_final_snapshot" {
+variable "skip_final_snapshot" {
   type = bool
   description = "(Optional) Determines whether a final DB snapshot is created before the DB instance is deleted. If true is specified, no DBSnapshot is created. If false is specified, a DB snapshot is created before the DB instance is deleted, using the value from final_snapshot_identifier. Default is false."
   default = false
 }
 
-variable "rds_final_snapshot_identifier" {
+variable "final_snapshot_identifier" {
   type = string
   description = "(Optional) The name of your final DB snapshot when this DB instance is deleted. Must be provided if skip_final_snapshot is set to false."
   default = false
